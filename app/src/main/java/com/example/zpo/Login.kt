56b6f832d70bac3be6da4d4df34251bde08fcd15
Julian268398 <268398@student.pwr.edu.pl>
2025-04-02 23:39:34 +0200

    Przechodzenie między layoutami

--- app/src/main/java/com/example/zpo/Login.kt
+++ app/src/main/java/com/example/zpo/Login.kt
@@ -1,4 +1,28 @@
 package com.example.zpo
 
-class Login {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_layout)
+
+        val loginButton = findViewById<Button>(R.id.ButtonLogIn)
+        val registerButton = findViewById<Button>(R.id.buttonRegister)
+
+        // Obsługa kliknięcia przycisku Log in
+        loginButton.setOnClickListener {
+            val intent = Intent(this, main_page::class.java)
+            startActivity(intent)
+        }
+
+        // Obsługa kliknięcia przycisku Register
+        registerButton.setOnClickListener {
+            val intent = Intent(this, Register::class.java) // Upewnij się, że masz klasę Register
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/com/example/zpo/Register.kt
+++ app/src/main/java/com/example/zpo/Register.kt
@@ -1,4 +1,26 @@
 package com.example.zpo
 
-class Register {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_layout)
+
+        val registerButton = findViewById<Button>(R.id.ButtonCreatAccount)
+        val backToLoginButton = findViewById<Button>(R.id.buttonBackToLogIn)
+
+        val intent = Intent(this, Login::class.java)
+
+        registerButton.setOnClickListener {
+            startActivity(intent)
+        }
+
+        backToLoginButton.setOnClickListener {
+            startActivity(intent)
+        }
+    }
 }
